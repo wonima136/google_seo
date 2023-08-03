abe01eb602b630a7c80cfbeb211bca70ca4e6bfa
wonima136 <100024933+wonima136@users.noreply.github.com>
2023-08-03 21:16:53 +0800

    Update python3.9.sh

--- python3.9.sh
+++ python3.9.sh
@@ -38,20 +38,25 @@ touch /root/version
 echo "查询nginx是否运行"
 # 查询nginx是否运行
 systemctl status nginx
+echo ""
 
 # 查看nginx版本
 echo "nginx当前版本"
 nginx -v
+echo ""
 
 # 查看python版本
 echo "python当前版本"
 python3 --version
+echo ""
 
 # 显示Hugo版本
 echo "Hugo当前版本"
 hugo version
+echo ""
 
 echo "查看nginx配置"
 cat /root/nginx_config.txt
+echo ""
 
 echo "关于python安装完成出现的错误可以忽略他"

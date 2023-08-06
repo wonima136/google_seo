f7111200f12bdcf6b8e51ac5adaec418e28bd6c3
wonima136 <100024933+wonima136@users.noreply.github.com>
2023-08-06 21:56:40 +0800

    Update program_download.sh

--- program_download.sh
+++ program_download.sh
@@ -2,28 +2,28 @@
 BLUE=$(tput setaf 4)
 NORMAL=$(tput sgr0)
 
-# 创建wwwroot文件
-sudo mkdir -p  /home/wwwroot
+# 创建wwwroot文件夹
+sudo mkdir -p /home/wwwroot
 
-# 进入项目
-cd /home/wwwroot/
+# 进入项目目录
+cd /home/wwwroot
 
 # 安装svn
-yum install svn -y
+sudo yum install svn -y
 
-# 下载hogo程序
+# 下载hugo程序
 sudo svn checkout https://github.com/wonima136/google_seo/trunk/hugo
-echo "下载hogo程序完成..."
+echo "下载hugo程序完成..."
 
 # 下载脚本
 sudo svn checkout https://github.com/wonima136/google_seo/trunk/python
 echo "下载配套python脚本完成..."
 
-# 给wwwroot最高权限权限
+# 赋予wwwroot文件夹最高权限
 sudo chmod 777 /home/wwwroot/*
 
 clear
 
 echo "${BLUE}资料准备完成${NORMAL}"
-echo "${BLUE}在content.py配置数据库信息${NORMAL}"
-echo "${BLUE}domains.txt添加要上的顶级域名${NORMAL}"
+echo "${BLUE}请在content.py中配置数据库信息${NORMAL}"
+echo "${BLUE}请在domains.txt中添加要上的顶级域名${NORMAL}"

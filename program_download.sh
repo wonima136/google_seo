eb0400ac54f6b07d5c501ceadaa1bf23bb4e0228
wonima136 <100024933+wonima136@users.noreply.github.com>
2023-08-06 22:03:42 +0800

    Update program_download.sh

--- program_download.sh
+++ program_download.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-BLUE=$(tput setaf 4)
-NORMAL=$(tput sgr0)
+# BLUE=$(tput setaf 4)
+# NORMAL=$(tput sgr0)
 
 # 创建wwwroot文件夹
-sudo mkdir -p /home/wwwroot
+# sudo mkdir -p /home/wwwroot
 
 # 进入项目目录
-cd /home/wwwroot
+# cd /home/wwwroot
 
 # 安装svn
-sudo yum install svn -y
+# sudo yum install svn -y
 
 # 下载hugo程序
-sudo svn checkout https://github.com/wonima136/google_seo/trunk/hugo
-echo "下载hugo程序完成..."
+# sudo svn checkout https://github.com/wonima136/google_seo/trunk/hugo
+# echo "下载hugo程序完成..."
 
 # 下载脚本
-sudo svn checkout https://github.com/wonima136/google_seo/trunk/python
-echo "下载配套python脚本完成..."
+# sudo svn checkout https://github.com/wonima136/google_seo/trunk/python
+# echo "下载配套python脚本完成..."
 
 # 赋予wwwroot文件夹下所有文件最高权限
-sudo chmod 777 /home/wwwroot/*
+# sudo chmod 777 /home/wwwroot/*
 
 # 赋予wwwroot文件夹最高权限
-sudo chmod 777 /home/wwwroot
+# sudo chmod 777 /home/wwwroot
 
 clear
 

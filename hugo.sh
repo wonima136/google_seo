b3da234da20ca4da75e5517844279c5976befe72
wonima136 <100024933+wonima136@users.noreply.github.com>
2023-08-06 22:53:29 +0800

    Update hugo.sh

--- hugo.sh
+++ hugo.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # 安装Go
-yum install -y golang
+# yum install -y golang
 
 
 # 添加Hugo的yum源文件
-cat << EOF > /etc/yum.repos.d/hugo.repo
-[daftaupe-hugo]
-name=Copr repo for hugo owned by daftaupe
-baseurl=https://copr-be.cloud.fedoraproject.org/results/daftaupe/hugo/epel-7-\$basearch/
-type=rpm-md
-skip_if_unavailable=True
-gpgcheck=1
-gpgkey=https://copr-be.cloud.fedoraproject.org/results/daftaupe/hugo/pubkey.gpg
-repo_gpgcheck=0
-enabled=1
-EOF
+# cat << EOF > /etc/yum.repos.d/hugo.repo
+# [daftaupe-hugo]
+# name=Copr repo for hugo owned by daftaupe
+# baseurl=https://copr-be.cloud.fedoraproject.org/results/daftaupe/hugo/epel-7-\$basearch/
+# type=rpm-md
+# skip_if_unavailable=True
+# gpgcheck=1
+# gpgkey=https://copr-be.cloud.fedoraproject.org/results/daftaupe/hugo/pubkey.gpg
+# repo_gpgcheck=0
+# enabled=1
+# EOF
 
 # 安装Hugo
-yum -y install hugo
+sudo yum reinstall hugo -y
 
 
 # 开始安装python3.9

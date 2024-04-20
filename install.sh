c000cfa52d612d4ad111709a6ab03bc6318109ff
wonima136 <100024933+wonima136@users.noreply.github.com>
2024-04-20 16:53:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,57 +1,40 @@
 #!/bin/bash
-#
-#   Dante Socks5 Server AutoInstall
-#   -- Owner:       https://www.inet.no/dante
-#   -- Provider:    https://sockd.info
-#   -- Author:      Lozy
-#
 
-# Check if user is root
-if [ $(id -u) != "0" ]; then
-    echo "Error: You must be root to run this script, please use root to install"
-    exit 1
-fi
+# 安装EPEL和Remi仓库
+sudo yum install -y epel-release yum-utils
+sudo yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+sudo yum-config-manager --enable remi-php74
 
-REQUEST_SERVER="https://raw.github.com/Lozy/danted/master"
-SCRIPT_SERVER="https://public.sockd.info"
-SYSTEM_RECOGNIZE=""
+# 更新软件包索引
+sudo yum update -y
 
-[ "$1" == "--no-github" ] && REQUEST_SERVER=${SCRIPT_SERVER}
+# 安装Nginx
+sudo yum install -y nginx
 
-if [ -s "/etc/os-release" ];then
-    os_name=$(sed -n 's/PRETTY_NAME="\(.*\)"/\1/p' /etc/os-release)
+# 启动Nginx服务并设置为开机自启
+sudo systemctl start nginx
+sudo systemctl enable nginx
 
-    if [ -n "$(echo ${os_name} | grep -Ei 'Debian|Ubuntu' )" ];then
-        printf "Current OS: %s\n" "${os_name}"
-        SYSTEM_RECOGNIZE="debian"
+# 安装MySQL
+sudo yum install -y mysql-server
 
-    elif [ -n "$(echo ${os_name} | grep -Ei 'CentOS')" ];then
-        printf "Current OS: %s\n" "${os_name}"
-        SYSTEM_RECOGNIZE="centos"
-    else
-        printf "Current OS: %s is not support.\n" "${os_name}"
-    fi
-elif [ -s "/etc/issue" ];then
-    if [ -n "$(grep -Ei 'CentOS' /etc/issue)" ];then
-        printf "Current OS: %s\n" "$(grep -Ei 'CentOS' /etc/issue)"
-        SYSTEM_RECOGNIZE="centos"
-    else
-        printf "+++++++++++++++++++++++\n"
-        cat /etc/issue
-        printf "+++++++++++++++++++++++\n"
-        printf "[Error] Current OS: is not available to support.\n"
-    fi
-else
-    printf "[Error] (/etc/os-release) OR (/etc/issue) not exist!\n"
-    printf "[Error] Current OS: is not available to support.\n"
-fi
+# 启动MySQL服务并设置为开机自启
+sudo systemctl start mysqld
+sudo systemctl enable mysqld
 
-if [ -n "$SYSTEM_RECOGNIZE" ];then
-    wget -qO- --no-check-certificate ${REQUEST_SERVER}/install_${SYSTEM_RECOGNIZE}.sh | \
-        bash -s -- $*  | tee /tmp/danted_install.log
-else
-    printf "[Error] Installing terminated"
-    exit 1
-fi
+# MySQL安装后自动设置root密码
+NEW_ROOT_PASSWORD='f7a910dfcb021ff'
+sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${NEW_ROOT_PASSWORD}'; FLUSH PRIVILEGES;"
 
-exit 0
+# 安装PHP及其与MySQL交互的扩展
+sudo yum install -y php php-mysqlnd php-fpm
+
+# 启动PHP-fpm服务并设置为开机自启
+sudo systemctl start php-fpm
+sudo systemctl enable php-fpm
+
+# 显示安装的服务状态
+echo "Nginx, MySQL, and PHP-7.4 have been installed and started."
+echo "Nginx version: $(nginx -v)"
+echo "MySQL service status: $(systemctl status mysqld | grep 'Active')"
+echo "PHP version: $(php -v | head -n 1)"

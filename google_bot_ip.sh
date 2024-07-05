2c127985c88de3faa435799fc9fb745ed7d6a88a
wonima136 <100024933+wonima136@users.noreply.github.com>
2024-07-05 15:50:51 +0800

    Update google_bot_ip.sh

--- google_bot_ip.sh
+++ google_bot_ip.sh
@@ -14,10 +14,7 @@ fi
 curl https://www.cloudflare.com/ips-v4 | awk '{print "set_real_ip_from " $1 ";"}' > /www/googlebot_ips/cloudflare_ips_v4.txt && chmod 777 /www/googlebot_ips/cloudflare_ips_v4.txt
 
 # 从 Google JSON 数据获取地址，如果非空且有效，则添加'allow'到文件中
-curl https://developers.google.com/search/apis/ipranges/googlebot.json | jq -r '.prefixes[] | select(.ipv4Prefix) | .ipv4Prefix' | while read -r ip; do if [[ $ip != "null" ]]; then echo "allow $ip;" >> /www/googlebot_ips/googlebot_ips.txt; fi; done
-
-# 从提供的URL获取IP，并保存到文件中，并添加 "allow" 前缀和 ";" 后缀
-curl https://www.addlink.lol/data/ip.txt | awk '{print "allow " $1 ";"}' > /www/googlebot_ips/whitelist_ips.txt
+curl https://developers.google.com/search/apis/ipranges/googlebot.json | jq -r '.prefixes[] | .ipv4Prefix, .ipv6Prefix' | while read -r ip; do if [[ $ip != "null" ]]; then echo "allow $ip;" >> /www/googlebot_ips/googlebot_ips.txt; fi; done
 
 # 将文件权限设置为777
 chmod 777 /www/googlebot_ips/*

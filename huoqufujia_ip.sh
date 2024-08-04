14d9e233806bb404b1bc6fa692763047467d7077
wonima136 <100024933+wonima136@users.noreply.github.com>
2024-08-04 16:17:32 +0800

    Create huoqufujia_ip.sh

---
+++ huoqufujia_ip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 保存IP段的文件路径
+output_file="/www/wwwroot/data/服务器全部IP.txt"
+
+# 清空文件内容，如果文件存在的话
+> "$output_file"
+
+# 获取 eth0 网络接口上的公网IP地址并保存到文件
+echo "获取 eth0 网络接口上的公网IP地址..."
+/sbin/ip addr show dev eth0 | grep -oP 'inet \K[\d.]+' | sort -u >> "$output_file"
+
+echo "附加IP地址获取完成，保存在 $output_file 中。"

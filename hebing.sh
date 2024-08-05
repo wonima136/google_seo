342fa09884ef5280dc8170c3b74d40f04551f715
wonima136 <100024933+wonima136@users.noreply.github.com>
2024-08-05 14:54:44 +0800

    Update hebing.sh

--- hebing.sh
+++ hebing.sh
@@ -1,5 +1,8 @@
+### 每台服务器只要开启ip拦截的必须创建任务计划2分钟运行一次
 bash <(curl -s https://raw.githubusercontent.com/wonima136/google_seo/main/updata_ip.sh)
+
 
 sleep 5
 
-bash <(curl -s https://raw.githubusercontent.com/wonima136/google_seo/main/logs-time.sh)
+### 谷歌爬虫IP白名单，一天运行一次
+bash <(curl -s https://raw.githubusercontent.com/wonima136/google_seo/main/google_bot_ip.sh)

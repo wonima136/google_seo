db8961cb5850233e0071b9f9e44aad3c48684fc3
wonima136 <100024933+wonima136@users.noreply.github.com>
2024-08-05 14:52:17 +0800

    Create hebing.sh

---
+++ hebing.sh
@@ -0,0 +1,5 @@
+bash <(curl -s https://raw.githubusercontent.com/wonima136/google_seo/main/updata_ip.sh)
+
+sleep 5
+
+bash <(curl -s https://raw.githubusercontent.com/wonima136/google_seo/main/logs-time.sh)

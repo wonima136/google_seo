88c1cb6779e94d8d8f834c89f7fb4d4067e3d586
wonima136 <100024933+wonima136@users.noreply.github.com>
2024-08-23 18:29:03 +0800

    Create hebing_no_bing.sh

---
+++ hebing_no_bing.sh
@@ -0,0 +1,7 @@
+### 谷歌爬虫IP
+bash <(curl -s https://raw.githubusercontent.com/wonima136/google_seo/main/google_bot_ip_no_bing.sh)
+
+sleep 5
+
+### 网站logs
+bash <(curl -s https://raw.githubusercontent.com/wonima136/google_seo/main/logs-time.sh)

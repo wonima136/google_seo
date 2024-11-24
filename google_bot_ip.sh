a2c5b6bdda8a6696f760fb7f997ee281e9e68b92
wonima136 <100024933+wonima136@users.noreply.github.com>
2024-11-25 03:45:13 +0800

    Update google_bot_ip.sh

--- google_bot_ip.sh
+++ google_bot_ip.sh
@@ -5,7 +5,7 @@ if [ ! -d "/www/googlebot_ips/" ]; then
   mkdir -p /www/googlebot_ips/
 fi
 
-# 检查jq是否已经安装。如果没有安装，则执行安装
+# 检查 jq 是否已经安装。如果没有安装，则执行安装
 if ! [ -x "$(command -v jq)" ]; then
   sudo yum install jq -y
 fi
@@ -22,7 +22,7 @@ curl -s https://www.cloudflare.com/ips-v6 | awk '{print "set_real_ip_from " $1 "
 # 设置 cloudflare_ips_v4.txt 的权限
 chmod 777 /www/googlebot_ips/cloudflare_ips_v4.txt
 
-# 从 Google JSON 数据获取地址，如果非空且有效，则追加到临时文件中
+# 从 Google JSON 数据获取 IPv4 和 IPv6 地址，如果非空且有效，则追加到临时文件中
 curl -s https://developers.google.com/search/apis/ipranges/googlebot.json | jq -r '.prefixes[] | .ipv4Prefix, .ipv6Prefix' | while read -r ip; do
   if [[ $ip != "null" ]]; then
     echo "allow $ip;" >> "$temp_file"
@@ -32,18 +32,18 @@ done
 # 清空 googlebot_ips.txt 文件，以便重新写入所有数据
 > /www/googlebot_ips/googlebot_ips.txt
 
-# 从 Bing JSON 数据获取地址，如果非空且有效，则追加到 googlebot_ips.txt 中
+# 将临时文件内容（包括 IPv4 和 IPv6 地址）追加到 googlebot_ips.txt 中
+cat "$temp_file" >> /www/googlebot_ips/googlebot_ips.txt
+
+# 从 Bing JSON 数据获取 IPv4 地址，如果非空且有效，则追加到 googlebot_ips.txt 中
 curl -s https://www.bing.com/toolbox/bingbot.json | jq -r '.prefixes[] | select(.ipv4Prefix != null) | .ipv4Prefix' | while read -r ip; do
   if [[ $ip != "null" ]]; then
     echo "allow $ip;" >> /www/googlebot_ips/googlebot_ips.txt
   fi
 done
 
-# 将临时文件覆盖到 googlebot_ips.txt
-cat "$temp_file" >> /www/googlebot_ips/googlebot_ips.txt
-
 # 删除临时文件
 rm "$temp_file"
 
-# 将文件权限设置为777
+# 将文件权限设置为 777
 chmod 777 /www/googlebot_ips/*
